3ad7153da4d7814ec90a0d713c2704b1cd4c46b7
DionPereira-com <info@dionpereira.com>
2026-03-02 13:16:53 +0800

    Update App.tsx
    aanpak-bg overlay 80 to 90

--- src/App.tsx
+++ src/App.tsx
@@ -328,7 +328,7 @@ export default function App() {
           {/* Background image */}
   <div className="absolute inset-0 z-0">
     <img src="/aanpak-bg.webp" className="w-full h-full object-cover" />
-    <div className="absolute inset-0 bg-deep-navy/80" />
+    <div className="absolute inset-0 bg-deep-navy/90" />
   </div>  
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-[radial-gradient(circle_at_center,rgba(233,69,96,0.05)_0%,transparent_70%)]"></div>
           
